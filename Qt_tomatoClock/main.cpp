6bf87576d97be03ef4bb901774aa0051139ed038
Crescent746 <164714039+Crescent746@users.noreply.github.com>
2024-05-31 02:28:34 +0800

    Add files via upload

---
+++ Qt_tomatoClock/main.cpp
@@ -0,0 +1,12 @@
+#include "tomatoclock.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    TomatoClock w;
+    w.show();
+    return a.exec();
+}
+
